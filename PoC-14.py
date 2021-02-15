1b4814bd0811f829eb487a027de04ee2e8715cb9
Scott Schoeller <SchoelleSJ01@uww.edu>
2021-02-15 13:25:48 -0600

    Fixed "PoC-14.py"

--- PoC-14.py
+++ PoC-14.py
@@ -14,7 +14,8 @@ class searchDNA:
         self.S = S
         self.binPos = 1
         self.currentBit = 0
-        self.binCtr = 0x00000000
+        self.binCtr = 0x0000000
+        self.binFlag = False 
         self.binList = [ ]
         self.startList = [ ]
         self.stopList = [ ]
@@ -27,10 +28,12 @@ class searchDNA:
     def binCompress(self): # traslates to compressed version; adds to list
         self.binCtr = self.binCtr << 4
         self.binPos += 1
-        if self.binPos == 8: # end of 32 bits
+        if self.binFlag == True:
             self.binList.append(self.binCtr)
-            self.binCtr = 0x00000000
-            self.binPos = 1 # reset
+            self.binCtr = 0x0000000
+             # reset
+            self.binPos = 1
+            self.binFlag = False
 
     def binDecompress(self, binL): # binL is the list of 32-bit words
         return binL
@@ -54,50 +57,50 @@ class searchDNA:
                 self.binCtr = self.binCtr | 4 # 2^2
                 self.binCompress()
             elif char == 'C':
+                self.binFlag == True
                 self.binCompress()
             elif char == 'a' or char == 't' or char == 'c' or char == 'g': # inactive regions
+                self.binFlag == True
+                self.binCompress()
+            if self.binPos/28 == 1 or i == len(self.S)-1: # i term important when len(S) - 1 <
+                self.binFlag = True
                 self.binCompress()
             #else:
                 #self.binCompress()
         
         binShift = 0 # counts every four bits
-        mask = 0x0000000F # covers four bits at a time
+        mask = 0x0000000F # covers four bits at a time; an int in python is 30 bits
         pos = 0 # starting position in S
         bitPos = 0 # bit subscript
-        for j in range(0, len(self.binList)):
-            if pos < 8:
-                pos = j
-            else:
-                pos = j + (j-8) 
-            
-            Codon = hash(self.S[pos-2:pos+1])
-            if binShift < 32:
-                mask << 4
+        for j in range(0, len(S)-3): # len(S) - 4 end 
+            pos = j//28 # convert j into the respective position in binList
+            Codon = hash(self.S[j:j+3])
+            if binShift < 30:
                 bitPos += 1
             else:
                 # reset
-                binShift = 32
+                binShift = 0
                 bitPos = 0
-            if self.binList[j] << 4 & mask == 7: # Check for all codons due to false negative results on "TAA"
+            if self.binList[pos] >> (j*4) & 0x00000007 % 7 == 0: # Check for all codons due to false negative results on "TAA"
                 if Codon == self.TAA or Codon == self.TGA or Codon == self.TAG:
                     self.stopList.append(j)
-                    binShift += 4
+                    binShift += 1
                 elif Codon == self.ATG:
                     self.startList.append(j)
-                    binShift += 4
+                    binShift += 1
                 elif Codon == self.TAA:
                     self.stopList.append(j)
-                    binShift += 4
+                    binShift += 1
                 else:
-                    binShift += 4
-            elif self.binList[j] << 4 & mask << bitPos == 3 and Codon == self.TAA:
+                    binShift += 1
+            elif self.binList[pos] >> (j*4) & 0x00000003 % 3 == 0 and Codon == self.TAA:
                 self.stopList.append(j)
-                binShift += 4
+                binShift += 1
             else:
-                binShift += 4
+                binShift += 4 # skip to next four bits
                 continue # optimization
 
-        return [self.startList, self.stopList]
+        return [self.startList, self.stopList, self.S]
 
 with open(f, "r") as DNAfile:
     S = DNAfile.read()
